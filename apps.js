53d85f8d25e4eeb05417e0cd74408833fd51b4e4
ibnex <ibnex54@gmail.com>
2024-11-06 19:11:42 +0530

    op temrary comment

---
+++ apps.js
@@ -0,0 +1,12 @@
+function m4(){
+    let username=document.getElementById("username").value 
+    let password=document.getElementById("password").value 
+
+    if (username=="ibneahad" &&  password=="123456"){
+        alert("login successfully")
+   
+}
+else{
+    alert("login failed")
+
+}}
